371f7a017325b044a1022c42d301ce9afcce58ac
Loc Phan <pptloc28@Gmail.com>
2022-05-15 13:15:23 +0700

    update firebase

--- scripts/init-firebase.js
+++ scripts/init-firebase.js
@@ -1,2 +1,11 @@
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "drinks"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+  console.log(doc.data());
+});
